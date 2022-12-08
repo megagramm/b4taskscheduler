bdeb9e4f396c4de37041d02245cf273011de2a05
Andrey Grey <megagramm@gmail.com>
2022-12-08 12:02:42 +0300

    Clearing code

--- b4taskscheduler.inc.php
+++ b4taskscheduler.inc.php
@@ -1,34 +1,37 @@
 <?php
 /**
- * B4COM (C) 2022
+ * B4Com (C) 2022
  * Andrey Grey megagramm@gmail.com
+ * Yuri Morin yurkiywork@gmail.com
+ *
+ * Менеджер задач. Запускает задачи по расписанию в несколько потоков.
  */
 
 class B4TaskScheduler
 {
-    
-    public function __construct()
-    {
-        $this->task_count_in_stack = 4; // кол-во одновременно работающих процессов
-        $this->process_runing_method = 'proc_open'; // Метод работы с процессами
+    protected $task_count_in_stack = 4; // кол-во одновременно работающих процессов
+    protected $process_runing_method = 'proc_open'; // Метод работы с процессами
 
-        $this->proc_errorlog_path = "b4_error-output.txt";
-        $this->errorlog_path = "b4_error.log";
+    protected static $proc_errorlog_path = "";
+    protected static $errorlog_path = "b4_error.log";
 
-        $this->task_array = []; // массив всех задач
-        $this->task_array_empty = false; //Если тру, не позволяем работать с массивом задач
-        $this->task_stack = []; // массив задач в работе, ограничен $task_count_in_stack
-        $this->tasks_completed = []; // массив выполненных задач
+    protected $task_array = []; // массив всех задач
+    protected $task_array_empty = false; //Если тру, не позволяем работать с массивом задач
+    protected $task_stack = []; // массив задач в работе, ограничен $task_count_in_stack
+    protected $tasks_completed = []; // массив выполненных задач
 
-        //подготовка proc_open
-        $this->descriptorspec = array(
-            0 => array("pipe", "r"),
-            1 => array("pipe", "w"),
-            2 => array("file", $this->proc_errorlog_path, "a"),
-        );
+    // Подключение к БД
+    private $host = 'localhost';
+    private $dbname = 'b4com';
+    private $username = 'b4com';
+    private $password = 'qwerty';
 
-        $this->permission();
-    }
+    //подготовка proc_open
+    public $descriptorspec = [
+        0 => ['pipe', 'r'],
+        1 => ['pipe', 'w'],
+        2 => ['file', 'b4_error-output.txt', "a"], // путь к лог-файлу ошибок proc_open
+    ];
 
     /**
      * Функция доступа переопределяется в дочернем классе
@@ -38,7 +41,13 @@ class B4TaskScheduler
     public function permission()
     {
     }
-
+    /**
+     * Пишет сообщение в лог
+     *
+     *  TODO: Добавить аргументы в функцию с озможностью логировать в БД
+     * @param string $text
+     * @return void
+     */
     protected function log_error(string $text)
     {
         $handle = fopen($this->errorlog_path, "a");
@@ -79,18 +88,21 @@ class B4TaskScheduler
             $this->task_array_empty = true;
             exit();
         }
+
         $file_data = trim(file_get_contents($file));
         if (empty($file_data)) {
             $this->send_message('error', 'File is empty');
             $this->task_array_empty = true;
             exit();
         }
+
         $array = json_decode($file_data, true);
         if (!is_array($array)) {
             $this->send_message('error', 'JSON doesn`t decoded');
             $this->task_array_empty = true;
             exit();
         }
+
         $this->task_array = $array;
     }
 
@@ -101,23 +113,24 @@ class B4TaskScheduler
      */
     public function load_from_db()
     {
-        $host = 'localhost';
-        $dbname = 'b4com';
-        $username = 'b4com';
-        $password = 'qwerty';
 
         $db_array = [];
 
         try {
-            $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+            $conn = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->username, $this->password);
             // echo "Connected to $dbname at $host successfully.";
         } catch (PDOException $pe) {
-            die("Could not connect to the database $dbname :" . $pe->getMessage());
+            die("Could not connect to the database $this->dbname :" . $pe->getMessage());
         }
         $res = $conn->query("SELECT id,command FROM taskscheduler;");
+
         while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
             $db_array[] = $row;
         }
+        if (empty($db_array)) {
+            $this->task_array_empty = true;
+        }
+
         $this->task_array = $db_array;
     }
 
@@ -130,6 +143,7 @@ class B4TaskScheduler
     {
         //берём в стек первые задачи
         $this->task_stack = array_slice($this->task_array, 0, $this->task_count_in_stack, true);
+
         //отсекаем у исходного массива взятые элементы
         array_splice($this->task_array, 0, $this->task_count_in_stack);
     }
@@ -145,50 +159,50 @@ class B4TaskScheduler
     {
         echo 'Stack id is ' . $task_stack_id . ', task_id is ' . $task_data['id'] . PHP_EOL;
         // print_r ($task_data);
+
         $task_data['process'] = proc_open($task_data['command'], $this->descriptorspec, $task_data['pipes']);
         $task_data['pipe1_data'] = '';
+
         if (!is_resource($task_data['process'])) {
             $this->send_message("error", "process is missing. Task {$task_data['id']} skipped");
             unset($this->task_stack[$task_stack_id]);
         }
+
         stream_set_blocking($task_data['pipes'][1], false);
+
         $task_data['status'] = proc_get_status($task_data['process']);
     }
 
     protected function add_task_in_stack()
     {
-        // Todo: НЕ РАБОТАЕТ, ПЕРЕПИСАТЬ
 
         //проверить не пустой ли исходный массив
         if (empty($this->task_array)) {
             return;
         }
 
-        //добавить элементы в стек
-        //берём в стек первую задачу из task_array
+        // добавить элементы в стек
+        // берём в стек первую задачу из task_array
         $task = array_slice($this->task_array, 0, 1, true);
-        // var_dump($task);
-        //отсекаем у исходного массива взятый элемент
+
+        // отсекаем у исходного массива взятый элемент
         array_splice($this->task_array, 0, 1);
 
         // добавляем в стек задачу
-        // print_r($this->task_array);
-        // print_r($task[0]);
         $this->task_stack[] = $task[0];
-        
+
         // правильно берем последний элементы стека для создания процесса
-        $last_id_of_task_stack = array_keys($this->task_stack)[count($this->task_stack)-1];
-        // var_dump($this->task_stack);
+        $last_id_of_task_stack = array_keys($this->task_stack)[count($this->task_stack) - 1];
+
         // запустить процесс
-        // echo 'start make'.PHP_EOL;
         $this->make_proc($this->task_stack[$last_id_of_task_stack], $last_id_of_task_stack);
-        // echo 'stop make'.PHP_EOL;
-        // exit;
     }
     /**
      * Проверяет массив перед запуском. Правильная ли структура.
      * Неподходящие элементы убирает.
      *
+     * TODO: Должен писать в лог отдельно от метода send_message
+     * TODO: Должен писать в лог БД что задача не пошла в работу
      * @return void
      */
     public function check_task_array()
@@ -205,14 +219,12 @@ class B4TaskScheduler
                 unset($this->task_array[$task_array_id]);
             }
 
-            // if (!isset($task_data['date'])) {
-            //     $this->send_message('error', "Task  {$task_data['id']}: date is missing");
-            //     unset($this->task_array[$task_array_id]);
-            // }if (empty($this->task_array)) {
-			$this->send_message('error', 'Task array is empty');
-			exit();
-		}
+            if (empty($this->task_array)) {
+                $this->send_message('error', 'Task array is empty');
+                exit();
+            }
         }
+    }
 
     public function run_procs()
     {
@@ -220,6 +232,7 @@ class B4TaskScheduler
             $this->send_message('error', 'run_proc failed to start');
             exit();
         }
+        $this->check_task_array();
 
         $this->make_task_stack();
 
@@ -230,24 +243,20 @@ class B4TaskScheduler
             }
         }
 
-        $i = 0;
         if (isset($this->task_stack) && !empty($this->task_stack)) {
             while (!empty($this->task_stack)) {
                 foreach ($this->task_stack as $task_stack_id => &$task_data) {
-                    // echo $i . PHP_EOL;
-                    $i++;
 
                     if (!feof($task_data['pipes'][1])) {
-
                         $task_data['pipe1_data'] .= fgets($task_data['pipes'][1]);
                         echo fgets($task_data['pipes'][1]);
-                        // exit;
                     } else {
                         fclose($task_data['pipes'][0]);
                         echo 'Stack id(' . $task_stack_id . ') is closed, task_id is ' . $task_data['id'] . PHP_EOL;
                         fclose($task_data['pipes'][1]);
                         proc_close($task_data['process']);
                         unset($this->task_stack[$task_stack_id]);
+
                         //добавить следующую задачу
                         $this->add_task_in_stack();
                     }
