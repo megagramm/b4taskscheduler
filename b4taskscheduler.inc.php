941a009738fc939bc26e5403f2a00c2f412a4cd3
Andrey Grey <megagramm@gmail.com>
2022-12-09 11:54:57 +0300

    Внес исправления и todo
    Внесены исрпавления и туду в большом колве. Рабочую версию выгрузил

--- b4taskscheduler.inc.php
+++ b4taskscheduler.inc.php
@@ -5,15 +5,42 @@
  * Yuri Morin yurkiywork@gmail.com
  *
  * Менеджер задач. Запускает задачи по расписанию в несколько потоков.
+ *
+ * //! Восклицательный знак выделяет красным
+ *
  */
 
 class B4TaskScheduler
 {
+    // Todo: Переделать добавление из json на добавление задач из определенной папки всех файлов json. Если в папке есть файл, добавляются задачи из него в БД. Исходный файл удаляется. Ошибочные файлы пишутся в лог.
+
+    // Todo: Добавить режим debug. Расширенные логи, в обычном режиме без них
+
+    // Todo: Сделать метод добавления задачи в бд
+
+    // Todo: Метод удаления задачи из бд (только те что не выполнены)
+
+    // Todo: Сделать метод постановки задачи на паузу
+
+    // Todo: Блокировка задач администраторм системы
+
+    // Todo: Заблокировать задачу в БД(одну (ид записи), список(ид записей), все(all)) . Блокируются только невыполненные
+
+    // Todo: Разблокировать задачу в БД(одну (ид записи), список(ид записей), все(all)) . Блокируются только невыполненные
+
+    // Todo: Клонировать задачу, если это не задача по крону
+
+    // Todo: Сделать в БД заглушки client_id
+
+    // Todo: Переделать методы с учетом привиллегий пользователей
+
     protected $task_count_in_stack = 4; // кол-во одновременно работающих процессов
     protected $process_runing_method = 'proc_open'; // Метод работы с процессами
+    protected $debug_mod = false; // Если True, будет больше сопроводительной информации в выводе и логах
 
-    protected static $proc_errorlog_path = "";
-    protected static $errorlog_path = "b4_error.log";
+    public $errorlog_path = "b4_error.log"; // Текстовый лог ошибок
+
+    protected $path_to_json_files = __DIR__ . '/tasks_jsons/' . PHP_EOL; // Путь к файлам с задачами для загрузки в БД
 
     protected $task_array = []; // массив всех задач
     protected $task_array_empty = false; //Если тру, не позволяем работать с массивом задач
@@ -25,6 +52,9 @@ class B4TaskScheduler
     private $dbname = 'b4com';
     private $username = 'b4com';
     private $password = 'qwerty';
+
+    private $SQL_status_all = "'ready','started','canceled','pause','inprogress','complete'"; // просто подсказка по статусам
+    private $SQL_status_work = "'ready','started','inprogress'"; // рабочие статусы
 
     //подготовка proc_open
     public $descriptorspec = [
@@ -44,7 +74,7 @@ class B4TaskScheduler
     /**
      * Пишет сообщение в лог
      *
-     *  TODO: Добавить аргументы в функцию с озможностью логировать в БД
+     *  TODO: Добавить аргументы в функцию с возможностью логировать в БД
      * @param string $text
      * @return void
      */
@@ -71,9 +101,8 @@ class B4TaskScheduler
         );
 
         echo json_encode($output) . PHP_EOL;
-        $this->log_error($message);
-
     }
+
     /**
      * Загружаем данные из json
      *
@@ -106,24 +135,39 @@ class B4TaskScheduler
         $this->task_array = $array;
     }
 
-    /**
-     * Загружает задачи с БД
-     *
-     * @return void
-     */
-    public function load_from_db()
+    public function DB_connect()
     {
-
-        $db_array = [];
-
         try {
             $conn = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->username, $this->password);
             // echo "Connected to $dbname at $host successfully.";
+            return $conn;
         } catch (PDOException $pe) {
             die("Could not connect to the database $this->dbname :" . $pe->getMessage());
         }
-        $res = $conn->query("SELECT id,command FROM taskscheduler;");
+    }
 
+    /**
+     * Загружает задачи из БД
+     *
+     * @return void
+     */
+    public function DB_get_tasks()
+    {
+        $db_array = [];
+
+        $conn = $this->DB_connect();
+
+        $res = $conn->query("
+
+        SELECT `id`, `command`, `cron`, `run_on`,`status`
+        FROM `taskscheduler`
+        WHERE
+            disabled = 0
+            AND block = 0
+            AND status  IN ({$this->SQL_status_work});
+
+        ");
+        
         while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
             $db_array[] = $row;
         }
@@ -157,7 +201,7 @@ class B4TaskScheduler
      */
     public function make_proc(&$task_data, &$task_stack_id)
     {
-        echo 'Stack id is ' . $task_stack_id . ', task_id is ' . $task_data['id'] . PHP_EOL;
+        echo 'Stack id is ' . $task_stack_id . ', task_id is ' . $task_data['id'] . ' command: ' . $task_data['command'] . PHP_EOL;
         // print_r ($task_data);
 
         $task_data['process'] = proc_open($task_data['command'], $this->descriptorspec, $task_data['pipes']);
@@ -173,6 +217,11 @@ class B4TaskScheduler
         $task_data['status'] = proc_get_status($task_data['process']);
     }
 
+    /**
+     * Добавляет новую задачу в стек
+     *
+     * @return void
+     */
     protected function add_task_in_stack()
     {
 
@@ -197,6 +246,7 @@ class B4TaskScheduler
         // запустить процесс
         $this->make_proc($this->task_stack[$last_id_of_task_stack], $last_id_of_task_stack);
     }
+
     /**
      * Проверяет массив перед запуском. Правильная ли структура.
      * Неподходящие элементы убирает.
@@ -207,25 +257,68 @@ class B4TaskScheduler
      */
     public function check_task_array()
     {
+        if (empty($this->task_array)) {
+            $this->send_message('error', 'Task array is empty');
+            exit();
+        }
+
         foreach ($this->task_array as $task_array_id => &$task_data) {
 
-            if (!isset($task_data['id'])) {
-                $this->send_message('error', "Array index {$task_array_id}: field is missing");
+            print_r($task_data);
+
+            if (!isset($task_data['id']) or empty($task_data['id'])) {
+                $this->send_message('error', "Array index {$task_array_id}: field 'id' is missing or empty");
                 unset($this->task_array[$task_array_id]);
+                continue;
             }
 
-            if (!isset($task_data['command'])) {
-                $this->send_message('error', "Task {$task_data['id']}: command field is missing");
+            if (!array_key_exists('command', $task_data) or empty($task_data['command'])) {
+                $this->send_message('error', "Task {$task_data['id']}: 'command' field is missing or empty");
                 unset($this->task_array[$task_array_id]);
+                continue;
             }
 
-            if (empty($this->task_array)) {
-                $this->send_message('error', 'Task array is empty');
-                exit();
+            if (!array_key_exists('run_on', $task_data)) {
+                $this->send_message('error', "Task {$task_data['id']}: 'run_on' field is missing");
+                unset($this->task_array[$task_array_id]);
+                continue;
             }
+
+            if (!array_key_exists('cron', $task_data)) {
+                $this->send_message('error', "Task {$task_data['id']}: 'cron' field is missing");
+                unset($this->task_array[$task_array_id]);
+                continue;
+            }
+
+            if (!array_key_exists('status', $task_data) or empty($task_data['status'])) {
+                $this->send_message('error', "Task {$task_data['id']}: 'status' field is missing or empty");
+                unset($this->task_array[$task_array_id]);
+                continue;
+            }
+
+            // print_r($this->task_array);
+            $now_datetime = date("Y-m-d H:i:00");
+            if (empty($task_data['cron']) and empty($task_data['run_on'])) {
+                // Todo: Выполняется разовая задача
+            } elseif (empty($task_data['cron']) and !empty($task_data['run_on']) and $task_data['run_on'] == $now_datetime) {
+                // Todo: Правильнее сделать выборку из БД, если подходящие услвоия
+            } elseif (empty($task_data['run_on']) and !empty($task_data['cron'])) {
+                // Todo: Выполняется задача по крону, если совпадает с условиями
+            } else {
+                // Todo: Записать в БД прочие ошибки и изменить статус задачи на canceled
+            }
+
+            // print_r($task_data);
+
+            // die();
         }
     }
 
+    /**
+     * Запускает процессы из стека задач в работу, закрывает выполненные и добавляет новые
+     *
+     * @return void
+     */
     public function run_procs()
     {
         if ($this->task_array_empty == true) {
@@ -279,11 +372,12 @@ class B4TaskScheduler
             $this->process_runing_method = 'proc_open';
         }
     }
+
     /**
      * Определяет каким методом работать с процессами:
      *     proc_open
      *     ptreads         нужен php с zts
-     *     parallel         нужен php с zts
+     *     parallel        нужен php с zts
      *
      * @return void
      */
@@ -300,10 +394,33 @@ class B4TaskScheduler
     }
 }
 
-$file = 'commands.json';
-$job = new B4TaskScheduler;
-$job->get_process_runing_method();
+/**
+ *  Тестирование скрипта
+ *  Включается, если запустить сам скрипт, без вложения по include, require
+ */
+if ($argv && $argv[0] && realpath($argv[0]) === __FILE__) {
+//start testing
 
-// $job->load_from_json($file);
-$job->load_from_db();
-$job->run_procs();
+    // Todo: Перед запуском скрипта, устанавливать задачи в БД в статус ready
+
+    class Tsk extends B4TaskScheduler
+    {
+    }
+
+    $file = 'commands.json';
+    $job = new Tsk;
+    // $job = new B4TaskScheduler;
+
+    // выясняем метод работы
+    // $job->get_process_runing_method();
+
+    //загружаем задачи из json
+    // $job->load_from_json($file);
+
+    // загружаем задачи из БД
+    $job->DB_get_tasks();
+
+    // запускаем на исполение
+    $job->run_procs();
+
+} // end testing
--- index.php
+++ index.php
@@ -1,1 +1,16 @@
 <?php
+// require __DIR__.'/'.'b4taskscheduler.inc.php';
+include(__DIR__.'/'.'b4taskscheduler.inc.php');
+
+class Ttt extends B4TaskScheduler
+{
+}
+
+$file = 'commands.json';
+$job = new Ttt;
+// $job = new B4TaskScheduler;
+$job->get_process_runing_method();
+
+// $job->load_from_json($file);
+$job->load_from_db();
+$job->run_procs();
